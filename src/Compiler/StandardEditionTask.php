00d1426516c60438fcd2eefc428c084beb2fd010
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-04-15 04:00:27 +0200

    Update compiler framework.

---
+++ src/Compiler/StandardEditionTask.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of tenside/standard-edition.
+ *
+ * (c) Christian Schiffler <https://github.com/discordier>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    tenside/core
+ * @author     Christian Schiffler <https://github.com/discordier>
+ * @copyright  Christian Schiffler <https://github.com/discordier>
+ * @link       https://github.com/tenside/standard-edition
+ * @license    https://github.com/tenside/standard-edition/blob/master/LICENSE MIT
+ * @filesource
+ */
+
+namespace Tenside\StandardEdition\Compiler;
+
+use Symfony\Component\Finder\Finder;
+use Tenside\Compiler;
+use Tenside\Compiler\AbstractTask;
+
+/**
+ * This Compiler task adds all content from tenside/standard-edition into the phar.
+ *
+ * @author Christian Schiffler <https://github.com/discordier>
+ */
+class StandardEditionTask extends AbstractTask
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function compile()
+    {
+        $root = $this->getPackageRoot('tenside/standard-edition');
+
+        $finder = new Finder();
+        $finder->files()
+            ->ignoreVCS(true)
+            ->name('*.php')
+            ->notName('StandardEditionTask.php')
+            ->in($root . '/src');
+        foreach ($finder as $file) {
+            $this->addFile($file);
+        }
+
+        $this->addTensideBin();
+    }
+
+    /**
+     * Add the tenside main binary.
+     *
+     * @return void
+     */
+    private function addTensideBin()
+    {
+        $root = $this->getPackageRoot('tenside/standard-edition');
+
+        $content = file_get_contents($root . '/bin/tenside');
+        $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
+        $this->addFileContent('bin/tenside', $content);
+
+        $this->addFile(new \SplFileInfo($root . '/web/app.php'), true, 'web/app.php');
+    }
+}
