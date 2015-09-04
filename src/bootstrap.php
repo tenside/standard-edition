91d7308c52979c9899eff72e172517273cce10f2
Christian Schiffler <c.schiffler@cyberspectrum.de>
2015-09-05 00:44:30 +0200

    Change phpDoc

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of tenside/standard-edition.
  *
- * (c) Christian Schiffler <https://github.com/discordier>
+ * (c) Christian Schiffler <c.schiffler@cyberspectrum.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,10 +11,10 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    tenside/standard-edition
- * @author     Christian Schiffler <https://github.com/discordier>
- * @copyright  Christian Schiffler <https://github.com/discordier>
- * @link       https://github.com/tenside/standard-edition
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  2015 Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @license    https://github.com/tenside/standard-edition/blob/master/LICENSE MIT
+ * @link       https://github.com/tenside/standard-edition
  * @filesource
  */
 
